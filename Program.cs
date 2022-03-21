89a09186384b8e0a520fcce99f75cf58986d196a
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 01:49:25 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// A = Area b = Base H = altura
+
+double A, b, h;
+
+Console.WriteLine("digite a base do triangulo, por favor:");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite a altura do triangulo, por favor:");
+h = Convert.ToDouble(Console.ReadLine());
+
+A = (b * h) /2;
+
+Console.WriteLine($"A Area do triangulo é:{A:N3}");
